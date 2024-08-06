48c9759ce204b7a567214aa8c18912adbb121d57
Matt <107654917+MattSeltz@users.noreply.github.com>
2024-08-06 00:42:31 -0300

    Se progresa en el apartado "Chat"

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -1,9 +1,15 @@
+import { Usuarios } from "../models/usuarios.models.js";
+
 export const connection = (io) => {
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     console.log("Se ha conectado un usuario");
 
-    socket.on("message", (msg, id) => {
-      io.emit("message", { msg, id: socket.id });
+    const id = socket.handshake.auth.token;
+
+    const usuarios = await Usuarios.findById(id);
+
+    socket.on("message", (msg) => {
+      io.emit("message", { msg, id: usuarios });
     });
 
     socket.on("disconnect", () => console.log("Se ha desconectado un usuario"));
